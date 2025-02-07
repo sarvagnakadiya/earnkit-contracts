3da9482efc511ee63d1a8bd8e89efe36fbaaacdd
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2025-02-07 19:21:37 +0530

    chore: some cleanup

--- script/deploy-campaign.sh
+++ script/deploy-campaign.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 source .env
 
-# Required parameters
-# TRUSTED_ADDRESS="0x..."  # Address that can pause/unpause
-# CLAIM_FEE="1000000000000000"  # 0.001 ETH in wei
-# TREASURY_ADDRESS="0x..."  # Treasury address to receive fees
-# SPONSORED_CLAIM_FEE="500000000000000"  # 0.0005 ETH in wei
-
 # Deploy to specified network
 forge script script/DeployCampaign.s.sol:DeployCampaign \
     --rpc-url ${RPC_URL} \
